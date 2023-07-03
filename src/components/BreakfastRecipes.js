ef50a3a6ff5c9a3ece0055765606e9c15d334d46
bouquet1 <bukuleta@gmail.com>
2023-07-02 21:28:52 -0700

    lunch recipes doesn't work

--- src/components/BreakfastRecipes.js
+++ src/components/BreakfastRecipes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { v4 as uuidv4} from 'uuid'
 
 function BreakfastRecipes({breakfast}) {
+    // destructure prop
     const {id, name, totalPrepTime, directions1, directions2, ingredients, image, nutritionFacts } = breakfast;
 
 return (
@@ -12,7 +13,7 @@ return (
     <h4>{totalPrepTime}</h4>
     <p>{directions1}</p>
     <p>{directions2}</p>
-    {/* map through ingredients array ti list them */}
+    {/* map through ingredients array to list them */}
     <li>{ingredients.map(ingredient =>  (<li key={uuidv4()}>{ingredient}</li>))}</li>
     <p>{nutritionFacts}</p>
 </section>
--- src/components/LunchRecipes.js
+++ src/components/LunchRecipes.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { v4 as uuidv4} from 'uuid'
 
-function LunchRecipes() {
+function LunchRecipes({lunch}) {
+const {id, name, totalPrepTime, directions1, directions2, ingredients, image, nutritionFacts } = lunch;
+
   return (
-    <div>LunchRecipes</div>
+    <section>
+        <h2>Lunch Recipes</h2>
+        <image src={image} alt={name} />
+        <h3>{name}</h3>
+        <h4>{totalPrepTime}</h4>
+        <p>{directions1}</p>
+        <p>{directions2}</p>
+        {/* map through ingredients array ti list them */}
+        <li>{ingredients.map(ingredient =>  (<li key={uuidv4()}>{ingredient}</li>))}</li>
+        <p>{nutritionFacts}</p>
+</section>
   )
 }
 
 export default LunchRecipes
+
